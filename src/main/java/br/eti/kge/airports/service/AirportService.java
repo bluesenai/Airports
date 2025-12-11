8c843abb6cfdc5d226a9c541997e821b1dea545d
DIT2B <Blue>
2025-12-11 08:54:32 -0300

    Término da Fase 5 –Update interface AirportRepository – Adicionado assinatura de método Airport findByIataCode(). -Update classe AirportService -Adicionado método findByIataCodeSearch. -Update classe AirportController – Adicionado endpoint iatacode/{iataCode}.

--- src/main/java/br/eti/kge/airports/service/AirportService.java
+++ src/main/java/br/eti/kge/airports/service/AirportService.java
@@ -40,4 +40,8 @@ public class AirportService {
         return resultDTO;
 
     }
+    public Airport findByIataCode(String iataCode) {
+        Airport result = airportRepository.findByIataCode(iataCode);
+        return result;
+    }
 }
